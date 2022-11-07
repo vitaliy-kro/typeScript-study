64e19dec8d97a775824c5a1fe64b4e6142bf8a1f
vitaliy-kro <krochakv6@gmail.com>
2022-11-07 20:15:48 +0200

    finished lesson 2

--- app.js
+++ app.js
@@ -1,9 +1,64 @@
-var button = document.querySelector("button");
-var input1 = document.getElementById("num1");
-var input2 = document.getElementById("num2");
-function add(num1, num2) {
+"use strict";
+exports.__esModule = true;
+var age;
+age = 50;
+var name;
+name = 'Max';
+var toggle;
+toggle = true;
+var empty;
+empty = null;
+var notInitialize;
+notInitialize = undefined;
+var callback = function (a) { return 100 + a; };
+callback(3);
+var anything;
+anything = 'Text';
+anything = {};
+var some;
+some = 'Text';
+var str;
+if (typeof some === 'string')
+    str = some;
+var person;
+person = ['Max', 21];
+var Status;
+(function (Status) {
+    Status[Status["LOADING"] = 0] = "LOADING";
+    Status[Status["READY"] = 1] = "READY";
+})(Status || (Status = {}));
+var page = {
+    load: Status.READY
+};
+if (page.load === Status.READY) {
+    console.log('Page is load');
+}
+else
+    console.log('Page is loading');
+var someInfo;
+var someStatus;
+function showMessage(message) {
+    console.log(message);
+}
+function calc(num1, num2) {
     return num1 + num2;
 }
-button.addEventListener("click", function () {
-    console.log(add(+input1.value, +input2.value));
-});
+function customError() {
+    throw new Error('Error');
+}
+var page1 = {
+    title: 'The awesome page',
+    likes: 100,
+    accounts: ['Max', 'Anton', 'Nikita'],
+    status: 'open',
+    details: {
+        createAt: '2021-01-01',
+        updateAt: '2021-05-01'
+    }
+};
+var page2 = {
+    title: 'Python or Js',
+    likes: 5,
+    accounts: ['Alex'],
+    status: 'close'
+};
--- app.ts
+++ app.ts
@@ -1,11 +1,84 @@
-const button = document.querySelector("button");
-const input1 = document.getElementById("num1")! as HTMLInputElement;
-const input2 = document.getElementById("num2")! as HTMLInputElement;
+let age: number;
+age = 50;
+let name: string;
+name = 'Max';
+let toggle: boolean;
+toggle = true;
+let empty: null;
+empty = null;
+let notInitialize: undefined;
+notInitialize = undefined;
+let callback = (a: number): number => 100 + a;
+callback(3);
 
-function add(num1: number, num2: number) {
+let anything: any;
+anything = 'Text';
+anything = {};
+
+let some: unknown;
+some = 'Text';
+
+let str: string;
+
+if (typeof some === 'string') str = some;
+
+let person: [string, number];
+person = ['Max', 21];
+
+enum Status {
+  LOADING,
+  READY,
+}
+
+const page = {
+  load: Status.READY,
+};
+if (page.load === Status.READY) {
+  console.log('Page is load');
+} else console.log('Page is loading');
+
+let someInfo: string | number;
+let someStatus: 'enable' | 'disable';
+
+function showMessage(message: string): void {
+  console.log(message);
+}
+
+function calc(num1: number, num2: number): number {
   return num1 + num2;
 }
 
-button.addEventListener("click", function () {
-  console.log(add(+input1.value, +input2.value));
-});
+function customError(): never {
+  throw new Error('Error');
+}
+
+type Page = {
+  title: string;
+  likes: number;
+  acounts: [];
+  status: 'open' | 'close';
+  details?: {
+    createAt: Date;
+    updateAt: Date;
+  };
+};
+
+const page1 = {
+  title: 'The awesome page',
+  likes: 100,
+  accounts: ['Max', 'Anton', 'Nikita'],
+  status: 'open',
+  details: {
+    createAt: '2021-01-01',
+    updateAt: '2021-05-01',
+  },
+};
+
+const page2 = {
+  title: 'Python or Js',
+  likes: 5,
+  accounts: ['Alex'],
+  status: 'close',
+};
+
+export {};
